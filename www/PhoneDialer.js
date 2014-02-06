d706187ddaba6f4b8a9ae3d35b79d68b3d252c1e
Matthew Encke <matthew@encke.cr>
2014-02-06 14:02:32 -0600

    Update PhoneDialer.js

--- www/PhoneDialer.js
+++ www/PhoneDialer.js
@@ -1,12 +1,10 @@
-cordova.define("cr.encke.phonedialer.PhoneDialer", function(require, exports, module) {
-	var CallDialer = {
-		dial: function( phnum )	{
-			if( device.platform == "iOS" )	{
-  		  		cordova.exec( null, null, "PhoneDialer", "dialPhone", [phnum] );
-			}	else	{
-				document.location = "tel:" + phnum;
-			}
+var CallDialer = {
+	dial: function( phnum )	{
+		if( device.platform == "iOS" )	{
+  	  		cordova.exec( null, null, "PhoneDialer", "dialPhone", [phnum] );
+		}	else	{
+			document.location = "tel:" + phnum;
 		}
 	}
-	module.exports = CallDialer;
-});
+}
+module.exports = CallDialer;
