154866557ded32fe3b9d44990880138dc36b3b73
Matthew Encke <matthew@encke.cr>
2014-02-06 14:39:35 -0600

    Update PhoneDialer.js

--- www/PhoneDialer.js
+++ www/PhoneDialer.js
@@ -1,11 +1,12 @@
 
-	var CallDialer = {
-		dial: function( phnum )	{
-			if( device.platform == "iOS" )	{
-  	  			cordova.exec( null, null, "PhoneDialer", "dialPhone", [phnum] );
-			}	else	{
-				document.location = "tel:" + phnum;
-			}
+	var exec = require('cordova/exec');
+	
+	var phonedialer = function( phnum )	{
+		if( device.platform == "iOS" )	{
+  	  		exec( null, null, "PhoneDialer", "dialPhone", [phnum] );
+		}	else	{
+			document.location = "tel:" + phnum;
 		}
-	}
-	module.exports = CallDialer;
+	};
+	
+	module.exports = phonedialer;
